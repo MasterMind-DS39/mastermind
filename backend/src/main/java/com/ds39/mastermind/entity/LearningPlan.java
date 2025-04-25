e76a0e889506589bb3492ab00d1b5ced5bced64b
Randunu21 <randunudissanayake21@gmail.com>
2025-04-26 03:59:00 +0530

    Created LearningPlan and Lesson entities

--- backend/src/main/java/com/ds39/mastermind/entity/LearningPlan.java
+++ backend/src/main/java/com/ds39/mastermind/entity/LearningPlan.java
@@ -1,5 +1,25 @@
 package com.ds39.mastermind.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+
+@Entity
 public class LearningPlan {
 
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Long createdByUserId;
+    private int upvotes = 0;
+
+    @OneToMany(mappedBy = "plan", cascade = jakarta.persistence.CascadeType.ALL)
+    private List<Lesson> lessons;
+
+    // Getters and Setters
+    
 }
